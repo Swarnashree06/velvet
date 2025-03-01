367565a6d1496a8f3d4f067fae5a006cdf226301
Swarnashree06 <Swarnacharya06@gmail.com>
2025-03-01 11:59:36 +0530

    'i-did-in-alt-text'

--- src/Pages/Error.jsx
+++ src/Pages/Error.jsx
@@ -3,7 +3,7 @@ import err from "../Assets/Images/error.png"
 
 export default function Error() {
   return <div>
-    <img src="https://media.licdn.com/dms/image/v2/C5112AQEw1fXuabCTyQ/article-inline_image-shrink_1500_2232/article-inline_image-shrink_1500_2232/0/1581099611064?e=1745452800&v=beta&t=ky17F_selGJPIoy3DvzXIIK8FLoxNFu4QqksXNoRdwM"></img>
+    <img src="https://i.pinimg.com/originals/a4/62/d1/a462d192479048db0f02f4466b900e0a.gifgit"  alt="hello git new branch 01-03-2025-s"></img>
 
   </div>;
 }
